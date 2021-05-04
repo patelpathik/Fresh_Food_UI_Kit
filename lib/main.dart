6a99d5784971bc7fa5fb895ea871eb7e0ba6aa32
patelpathik <patelpathik1995@gmail.com>
2021-05-05 05:18:32 +0530

    Route to `Store`, `Search`, `VoiceSearch`, `Product` added

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:fresh_food/theme/app_theme.dart';
 import 'package:fresh_food/utils/app_template.dart';
 import 'package:fresh_food/views/home/home_view.dart';
 import 'package:fresh_food/views/on_boarding/on_boarding_view.dart';
+import 'package:fresh_food/views/product/product_view.dart';
+import 'package:fresh_food/views/search/search_view.dart';
 import 'package:fresh_food/views/sign_in/sign_in.dart';
 import 'package:fresh_food/views/splash_screen/splash_screen.dart';
+import 'package:fresh_food/views/store/store_view.dart';
+import 'package:fresh_food/views/voice_search/voice_search_view.dart';
 
 import 'views/home/home_view.dart';
 
@@ -35,6 +39,18 @@ class MyApp extends StatelessWidget {
             break;
           case OnBoarding.TAG:
             page = OnBoarding();
+            break;
+          case Store.TAG:
+            page = Store();
+            break;
+          case Search.TAG:
+            page = Search();
+            break;
+          case VoiceSearch.TAG:
+            page = VoiceSearch();
+            break;
+          case Product.TAG:
+            page = Product();
             break;
           case HomeView.TAG:
             page = HomeView();
