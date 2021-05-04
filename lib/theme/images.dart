98359a0cb569b2669996f6035b57b5453d1b19a0
patelpathik <patelpathik1995@gmail.com>
2021-05-04 15:49:55 +0530

    `Fruits`, `ThemeIcons`, `OnBoarding` Images added

---
+++ lib/theme/images.dart
@@ -0,0 +1,87 @@
+class Fruits {
+  Fruits._();
+
+  static const String imagePath = "./assets/images/fruits/icon-";
+  static const String colorImagePath = "./assets/images/fruits/icon-colour-";
+
+  static const String AVOCADO = "${imagePath}avocado.png";
+  static const String AVOCADO_COLOURED = "${colorImagePath}avocado.png";
+  static const String BANANA = "${imagePath}banana.png";
+  static const String BANANA_COLOURED = "${colorImagePath}banana.png";
+  static const String APPLE = "${imagePath}apple.png";
+  static const String APPLE_COLOURED = "${colorImagePath}apple.png";
+  static const String GRAPES = "grapes.png";
+  static const String GRAPES_COLOURED = "${colorImagePath}grapes.png";
+  static const String KIWI = "${imagePath}kiwi.png";
+  static const String KIWI_COLOURED = "${colorImagePath}kiwi.png";
+  static const String LEMON = "${imagePath}lemon.png";
+  static const String LEMON_COLOURED = "${colorImagePath}lemon.png";
+  static const String ORANGE = "orange.png";
+  static const String ORANGE_COLOURED = "${colorImagePath}orange.png";
+  static const String PEACH = "${imagePath}peach.png";
+  static const String PEACH_COLOURED = "${colorImagePath}peach.png";
+  static const String PEAR = "${imagePath}pear.png";
+  static const String PEAR_COLOURED = "${colorImagePath}pear.png";
+  static const String PINEAPPLE = "${imagePath}pineapple.png";
+  static const String PINEAPPLE_COLOURED = "${colorImagePath}pineapple.png";
+  static const String STRAWBERRY = "${imagePath}strawberry.png";
+  static const String STRAWBERRY_COLOURED = "${colorImagePath}strawberry.png";
+  static const String WATERMELON = "${imagePath}watermelon.png";
+  static const String WATERMELON_COLOURED = "${colorImagePath}watermelon.png";
+}
+
+class ThemeIcon {
+  ThemeIcon._();
+
+  static const String imagePath = "./assets/images/icons/icon-";
+
+  static const String ADD_CART = "${imagePath}add-cart.svg";
+
+  static const String ACCOUNT = "${imagePath}account.svg";
+  static const String ARROW_SMALL = "${imagePath}arrow-small.svg";
+  static const String ARROW = "${imagePath}arrow.svg";
+  static const String CALENDAR = "${imagePath}calendar.svg";
+  static const String CART = "${imagePath}cart.svg";
+  static const String CHAT = "${imagePath}chat.svg";
+  static const String CHEVRON = "${imagePath}chevron.svg";
+  static const String CLOCK = "${imagePath}clock.svg";
+  static const String CLOSE = "${imagePath}close.svg";
+  static const String COOK = "${imagePath}cook.svg";
+  static const String DELIVERY = "${imagePath}delivery.svg";
+  static const String DIFFICULTY = "${imagePath}difficulty.svg";
+  static const String EDIT = "${imagePath}edit.svg";
+  static const String FAST_DELIVERY = "${imagePath}fast-delivery.svg";
+  static const String LOCATION = "${imagePath}location.svg";
+  static const String MAIL = "${imagePath}mail.svg";
+  static const String ORDERS = "${imagePath}orders.svg";
+  static const String PADLOCK = "${imagePath}padlock.svg";
+  static const String PAYMENT = "${imagePath}payment.svg";
+  static const String RECIPES = "${imagePath}recipes.svg";
+  static const String REFRESH = "${imagePath}refresh.svg";
+  static const String SEARCH = "${imagePath}search.svg";
+  static const String SECURE = "${imagePath}secure.svg";
+  static const String SELECT = "${imagePath}select.svg";
+  static const String SETTINGS = "${imagePath}settings.svg";
+  static const String SHARE = "${imagePath}share.svg";
+  static const String SIGN_OUT = "${imagePath}sign-out.svg";
+  static const String STORE = "${imagePath}store.svg";
+  static const String SUMMARY = "${imagePath}summary.svg";
+  static const String TRASH = "${imagePath}trash.svg";
+}
+
+class Images {
+  Images._();
+
+  static const String imagePath = "./assets/images/";
+
+  static const String onBoarding_1 =
+      "${imagePath}onboarding/graphic-onboarding-1.svg";
+  static const String onBoarding_2 =
+      "${imagePath}onboarding/graphic-onboarding-2.svg";
+
+  static const String onBoarding_Dark_1 =
+      "${imagePath}onboarding/graphic-onboarding-dark-1.svg";
+
+  static const String onBoarding_Dark_2 =
+      "${imagePath}onboarding/graphic-onboarding-dark-2.svg";
+}
