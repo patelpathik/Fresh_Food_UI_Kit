a3068c8859176b3f263188b508e4d9f3a3f550cf
patelpathik <patelpathik1995@gmail.com>
2021-05-07 01:20:51 +0530

    `WillPopScope` added

--- lib/views/home/home_view_mobile.dart
+++ lib/views/home/home_view_mobile.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:fresh_food/theme/app_theme.dart';
-import 'package:fresh_food/theme/images.dart';
 import 'package:fresh_food/utils/globals.dart';
 import 'package:fresh_food/utils/sizeconfig.dart';
 import 'package:fresh_food/views/recipe_post/recipe_post_view.dart';
 import 'package:fresh_food/views/recipes/recipes_view.dart';
 import 'package:fresh_food/views/search/search_view.dart';
 import 'package:fresh_food/views/store/store_view.dart';
+import 'package:fresh_food/widgets/bottom_navbar.dart';
 
 class HomeMobilePortrait extends StatefulWidget {
   const HomeMobilePortrait({Key key}) : super(key: key);
@@ -26,84 +24,31 @@ class _HomeMobilePortraitState extends State<HomeMobilePortrait> {
     super.initState();
   }
 
+  Future<bool> _onBackPressed() async {
+    if (index == 1) {
+      Globals.homeNavStackIndex.setValue(0);
+      return false;
+    } else if (index == 3) {
+      Globals.homeNavStackIndex.setValue(2);
+      return false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    List<Widget> content = [Store(), Search(), Recipes(), RecipePost()];
-    return Scaffold(
-      body: IndexedStack(
-        children: content,
-        index: index,
-      ),
-      bottomNavigationBar: Container(
-        alignment: Alignment.center,
-        height: AppBar().preferredSize.height,
-        width: SizeConfig.screenWidth * 0.8,
-        margin: EdgeInsets.fromLTRB(
-          SizeConfig.screenWidth * 0.1,
-          0,
-          SizeConfig.screenWidth * 0.1,
-          10,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            GestureDetector(
-              onTap: () => Globals.homeNavStackIndex.setValue(0),
-              child: SvgPicture.asset(
-                ThemeIcon.STORE,
-                color: index == 0 || index == 1
-                    ? COLORS.GREEN
-                    : COLORS.MEDIUM_DARK_GREY,
-              ),
-            ),
-            GestureDetector(
-              onTap: () => Globals.homeNavStackIndex.setValue(2),
-              child: SvgPicture.asset(
-                ThemeIcon.RECIPES,
-                color: index == 2 ? COLORS.GREEN : COLORS.MEDIUM_DARK_GREY,
-              ),
-            ),
-            Container(
-              height: AppBar().preferredSize.height * 0.8,
-              width: AppBar().preferredSize.height * 0.8,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  // color: COLORS.MEDIUM_DARK_GREY,
-                  // shape: BoxShape.circle,
-                  ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(AppBar().preferredSize.height * 0.8),
-                ),
-                child: Material(
-                  color: COLORS.MEDIUM_DARK_GREY,
-                  child: InkWell(
-                    child: Container(
-                      height: AppBar().preferredSize.height * 0.8,
-                      width: AppBar().preferredSize.height * 0.8,
-                      padding: EdgeInsets.all(
-                        AppBar().preferredSize.height * 0.2,
-                      ),
-                      child: SvgPicture.asset(
-                        ThemeIcon.PLUS,
-                        color: COLORS.WHITE,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SvgPicture.asset(
-              ThemeIcon.CART,
-              color: COLORS.MEDIUM_DARK_GREY,
-            ),
-            SvgPicture.asset(
-              ThemeIcon.SETTINGS,
-              color: COLORS.MEDIUM_DARK_GREY,
-            ),
-          ],
-        ),
+    List<Widget> content = [
+      Store(),
+      Search(),
+      Recipes(),
+      RecipePost(),
+    ];
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: Scaffold(
+        body: content[index],
+        bottomNavigationBar: BottomNavBar.bottomNavBar(context),
       ),
     );
   }
