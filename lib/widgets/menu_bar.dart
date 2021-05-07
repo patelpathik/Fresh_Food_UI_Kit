e0e2bb7dc73df2e2d606e346c55edb2decacfb0b
patelpathik <patelpathik1995@gmail.com>
2021-05-08 04:08:23 +0530

    `MenuBar` widget added

---
+++ lib/widgets/menu_bar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_food/theme/app_theme.dart';
+import 'package:fresh_food/utils/sizeconfig.dart';
+import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
+
+class MenuBar {
+  MenuBar._();
+
+  static double height = AppBar().preferredSize.height;
+  static double marginTB = 10;
+
+  static Widget menuBarFromText(
+    BuildContext context, {
+    @required List<String> options,
+    Preference<int> controller,
+  }) {
+    SizeConfig().init(context);
+    int selectedOptionInd = controller == null ? 0 : controller.getValue();
+
+    return StatefulBuilder(
+      builder: (context, setState) {
+        double width = SizeConfig.screenWidth * 0.9;
+        double marginLR = SizeConfig.screenWidth * 0.05;
+        double eleW = (width / options.length);
+        return Container(
+          child: Container(
+            height: height,
+            width: width,
+            margin: EdgeInsets.fromLTRB(marginLR, marginTB, marginLR, marginTB),
+            alignment: Alignment.center,
+            child: Stack(
+              children: [
+                /* bg */
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  curve: Curves.fastOutSlowIn,
+                  height: height,
+                  width: eleW,
+                  margin: EdgeInsets.only(
+                    top: 2,
+                    bottom: 2,
+                    left: selectedOptionInd * eleW,
+                  ),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: COLORS.GREEN,
+                    borderRadius: BorderRadius.all(Radius.circular(25)),
+                  ),
+                ),
+                /* options */
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: options.map(
+                    (e) {
+                      Color txtColor = selectedOptionInd == options.indexOf(e)
+                          ? COLORS.WHITE
+                          : COLORS.MEDIUM_GREY;
+                      return Container(
+                        height: height,
+                        margin: EdgeInsets.only(top: 2, bottom: 2),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              splashColor: COLORS.GREEN,
+                              onTap: () {
+                                setState(() {
+                                  selectedOptionInd = options.indexOf(e);
+                                });
+                                if (controller != null)
+                                  controller.setValue(selectedOptionInd);
+                              },
+                              child: Container(
+                                height: height,
+                                width: eleW,
+                                margin: EdgeInsets.only(top: 2, bottom: 2),
+                                alignment: Alignment.center,
+                                child: Text(
+                                  e,
+                                  style: TextStyle(
+                                    color: txtColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ).toList(),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
