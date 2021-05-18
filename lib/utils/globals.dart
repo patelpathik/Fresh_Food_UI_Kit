710a03f7264854de81adf663bc05d682545ec93d
patelpathik <patelpathik1995@gmail.com>
2021-05-19 04:17:50 +0530

    `isCustomThemeSet`, `customDarkModePref`, `checkoutMenuController` added

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -7,13 +7,27 @@ class Globals {
   static double menuBarH = AppBar().preferredSize.height + 20;
 
   static Preference<bool> isDarkMode;
+  static Preference<bool> isCustomThemeSet;
+  static Preference<bool> customDarkModePref;
   static Preference<int> homeNavStackIndex;
   static Preference<int> lastKnownHomeNavStackIndex;
   static Preference<int> quickShopMenuController;
+  static Preference<int> checkoutMenuController;
 
   static Future<bool> updatePrefs() async {
     final preference = await StreamingSharedPreferences.instance;
-    isDarkMode = preference.getBool("isDarkMode", defaultValue: false);
+    isDarkMode = preference.getBool(
+      "isDarkMode",
+      defaultValue: false,
+    );
+    isCustomThemeSet = preference.getBool(
+      "isCustomThemeSet",
+      defaultValue: false,
+    );
+    customDarkModePref = preference.getBool(
+      "customDarkModePref",
+      defaultValue: false,
+    );
     homeNavStackIndex = preference.getInt("homeNavStackIndex", defaultValue: 0);
     lastKnownHomeNavStackIndex = preference.getInt(
       "lastKnownHomeNavStackIndex",
@@ -21,6 +35,10 @@ class Globals {
     );
     quickShopMenuController = preference.getInt(
       "quickShopMenuController",
+      defaultValue: 0,
+    );
+    checkoutMenuController = preference.getInt(
+      "checkoutMenuController",
       defaultValue: 0,
     );
     return true;
