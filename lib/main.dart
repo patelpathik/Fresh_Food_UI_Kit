60d065bf81fae82bc19109a2f98127735837e01c
patelpathik <patelpathik1995@gmail.com>
2021-05-19 04:19:22 +0530

    Changes for custom theme preference & new routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fresh_food/theme/app_theme.dart';
 import 'package:fresh_food/utils/app_template.dart';
+import 'package:fresh_food/utils/globals.dart';
+import 'package:fresh_food/views/checkout/add_card/add_card.dart';
 import 'package:fresh_food/views/edit_quantity/edit_quantity_view.dart';
 import 'package:fresh_food/views/home/home_view.dart';
+import 'package:fresh_food/views/live_chat/live_chat_view.dart';
 import 'package:fresh_food/views/on_boarding/on_boarding_view.dart';
 import 'package:fresh_food/views/product/product_view.dart';
 import 'package:fresh_food/views/recipes/recipes_view.dart';
@@ -10,9 +13,12 @@ import 'package:fresh_food/views/search/search_view.dart';
 import 'package:fresh_food/views/sign_in/sign_in.dart';
 import 'package:fresh_food/views/splash_screen/splash_screen.dart';
 import 'package:fresh_food/views/store/store_view.dart';
+import 'package:fresh_food/views/user_account/user_account.dart';
+import 'package:fresh_food/views/user_orders/user_orders.dart';
 import 'package:fresh_food/views/voice_search/voice_search_view.dart';
 import 'package:swipeable_page_route/swipeable_page_route.dart';
 
+import 'views/checkout/checkout_view.dart';
 import 'views/home/home_view.dart';
 
 void main() {
@@ -20,15 +26,62 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.system;
+  bool isSet = false;
+
+  void listenToThemeChange() {
+    Globals.isCustomThemeSet.listen((value) {
+      ThemeMode themeMode = ThemeMode.system;
+      if ((Globals.customDarkModePref != null &&
+          Globals.isCustomThemeSet != null)) {
+        if (Globals.isCustomThemeSet.getValue()) {
+          themeMode = (Globals.customDarkModePref.getValue()
+              ? ThemeMode.dark
+              : ThemeMode.light);
+        } else {
+          themeMode = ThemeMode.system;
+        }
+      }
+      setState(() => this.themeMode = themeMode);
+      print("ThemeMode :: ${this.themeMode}");
+    });
+    Globals.customDarkModePref.listen((value) {
+      ThemeMode themeMode = ThemeMode.system;
+      if ((Globals.customDarkModePref != null &&
+          Globals.isCustomThemeSet != null)) {
+        if (Globals.isCustomThemeSet.getValue()) {
+          themeMode = (Globals.customDarkModePref.getValue()
+              ? ThemeMode.dark
+              : ThemeMode.light);
+        } else {
+          themeMode = ThemeMode.system;
+        }
+      }
+      setState(() => this.themeMode = themeMode);
+      print("ThemeMode :: ${this.themeMode}");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (Globals.customDarkModePref != null &&
+        Globals.isCustomThemeSet != null &&
+        !isSet) {
+      listenToThemeChange();
+      setState(() => isSet = true);
+    }
     return MaterialApp(
       title: 'Fresh Food',
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getAppTheme(),
       darkTheme: AppTheme().getDarkTheme(),
+      themeMode: themeMode,
       builder: (context, child) => AppTemplate(child: child),
       onGenerateRoute: (settings) {
         Widget page;
@@ -63,6 +116,21 @@ class MyApp extends StatelessWidget {
             break;
           case EditQuantity.TAG:
             page = EditQuantity();
+            break;
+          case Checkout.TAG:
+            page = Checkout();
+            break;
+          case AddCard.TAG:
+            page = AddCard();
+            break;
+          case UserAccount.TAG:
+            page = UserAccount();
+            break;
+          case UserOrders.TAG:
+            page = UserOrders();
+            break;
+          case LiveChat.TAG:
+            page = LiveChat();
             break;
         }
         return SwipeablePageRoute(
