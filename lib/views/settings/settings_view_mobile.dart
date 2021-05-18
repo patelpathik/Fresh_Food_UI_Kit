eeaeaa733c5950a799ededbc71ef1620c2197f44
patelpathik <patelpathik1995@gmail.com>
2021-05-19 04:13:09 +0530

    `Custom Theme` option & other events added

--- lib/views/settings/settings_view_mobile.dart
+++ lib/views/settings/settings_view_mobile.dart
@@ -4,10 +4,12 @@ import 'package:fresh_food/theme/app_theme.dart';
 import 'package:fresh_food/theme/images.dart';
 import 'package:fresh_food/utils/globals.dart';
 import 'package:fresh_food/utils/sizeconfig.dart';
+import 'package:fresh_food/views/live_chat/live_chat_view.dart';
+import 'package:fresh_food/views/user_account/user_account.dart';
+import 'package:fresh_food/views/user_orders/user_orders.dart';
 import 'package:fresh_food/widgets/appBar.dart';
 import 'package:fresh_food/widgets/bottom_curved_shadow.dart';
 
-import 'package:fresh_food/theme/images.dart';
 import 'package:fresh_food/widgets/switch.dart';
 
 class SettingsMobilePortrait extends StatefulWidget {
@@ -21,10 +23,24 @@ class _SettingsMobilePortraitState extends State<SettingsMobilePortrait> {
   bool isDark = false;
   int tappedDownInd = -1;
 
+  String currentDarkModePref = "";
+
   List<Setting> options = [
-    Setting(imagePath: ThemeIcon.ACCOUNT, title: "Your Account"),
-    Setting(imagePath: ThemeIcon.ORDERS, title: "Your Orders"),
-    Setting(imagePath: ThemeIcon.CHAT, title: "Live Chat"),
+    Setting(
+      imagePath: ThemeIcon.ACCOUNT,
+      title: "Your Account",
+      routePath: UserAccount.TAG,
+    ),
+    Setting(
+      imagePath: ThemeIcon.ORDERS,
+      title: "Your Orders",
+      routePath: UserOrders.TAG,
+    ),
+    Setting(
+      imagePath: ThemeIcon.CHAT,
+      title: "Live Chat",
+      routePath: LiveChat.TAG,
+    ),
   ];
 
   @override
@@ -42,6 +58,14 @@ class _SettingsMobilePortraitState extends State<SettingsMobilePortrait> {
     if (Globals.isDarkMode != null) {
       Globals.isDarkMode.listen((value) {
         setState(() => isDark = value);
+      });
+    }
+
+    if (Globals.isCustomThemeSet != null &&
+        Globals.customDarkModePref != null) {
+      setState(() {
+        currentDarkModePref =
+            Globals.isCustomThemeSet.getValue() ? "Custom" : "System";
       });
     }
 
@@ -90,26 +114,29 @@ class _SettingsMobilePortraitState extends State<SettingsMobilePortrait> {
       List<Widget> tiles = [];
 
       options.forEach((element) {
-        Widget tile = settingsTile(
-          leading: Container(
-            height: cardH,
-            width: cardH,
-            padding: EdgeInsets.all(cardH * 0.3),
-            child: SvgPicture.asset(
-              element.imagePath,
-              color: COLORS.GREEN,
-            ),
-          ),
-          title: element.title,
-          trailing: Container(
-            height: cardH,
-            width: cardH,
-            padding: EdgeInsets.all(cardH * 0.4),
-            child: RotatedBox(
-              quarterTurns: 3,
+        Widget tile = GestureDetector(
+          onTap: () => Navigator.pushNamed(context, element.routePath),
+          child: settingsTile(
+            leading: Container(
+              height: cardH,
+              width: cardH,
+              padding: EdgeInsets.all(cardH * 0.3),
               child: SvgPicture.asset(
-                ThemeIcon.CHEVRON,
-                color: COLORS.MEDIUM_DARK_GREY,
+                element.imagePath,
+                color: COLORS.GREEN,
+              ),
+            ),
+            title: element.title,
+            trailing: Container(
+              height: cardH,
+              width: cardH,
+              padding: EdgeInsets.all(cardH * 0.4),
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: SvgPicture.asset(
+                  ThemeIcon.CHEVRON,
+                  color: COLORS.MEDIUM_DARK_GREY,
+                ),
               ),
             ),
           ),
@@ -127,24 +154,52 @@ class _SettingsMobilePortraitState extends State<SettingsMobilePortrait> {
             color: COLORS.GREEN,
           ),
         ),
-        title: "Dark Mode",
+        title: "Theme Mode : $currentDarkModePref",
         trailing: Container(
           height: cardH,
           width: cardH,
           padding: EdgeInsets.all(10),
-          // child: ThemeSwitch(
-          //   value: !isDark,
-          //   onPress: () {
-          //     // Globals.isDarkMode.setValue(!isDark);
-          //     // debugPrint(Globals.isDarkMode.getValue().toString());
-          //     // setState(() => isDark = !isDark);
-          //     // AppTheme().switchTheme(isDark);
-          //   },
-          // ),
+          child: ThemeSwitch(
+            value: Globals.isCustomThemeSet.getValue(),
+            onPress: () => Globals.isCustomThemeSet
+                .setValue(!Globals.isCustomThemeSet.getValue()),
+          ),
         ),
       );
 
       tiles.add(darkModeTile);
+
+      Widget customDarkModeTile = AnimatedOpacity(
+        duration: Duration(milliseconds: 500),
+        opacity: Globals.isCustomThemeSet.getValue() ? 1 : 0,
+        child: settingsTile(
+          leading: Container(
+            height: cardH,
+            width: cardH,
+            padding: EdgeInsets.all(cardH * 0.3),
+            child: SvgPicture.asset(
+              ThemeIcon.BULB,
+              color: COLORS.GREEN,
+            ),
+          ),
+          title: "Dark Mode",
+          trailing: Container(
+            height: cardH,
+            width: cardH,
+            padding: EdgeInsets.all(10),
+            child: ThemeSwitch(
+              value: !Globals.customDarkModePref.getValue(),
+              onPress: () {
+                Globals.customDarkModePref
+                    .setValue(!Globals.customDarkModePref.getValue());
+                setState(() {});
+              },
+            ),
+          ),
+        ),
+      );
+
+      tiles.add(customDarkModeTile);
 
       return Column(
         mainAxisSize: MainAxisSize.min,
@@ -154,7 +209,10 @@ class _SettingsMobilePortraitState extends State<SettingsMobilePortrait> {
 
     Widget logOut() {
       return GestureDetector(
-        onTap: () => Navigator.of(context).pushReplacementNamed("/"),
+        // onTap: () => Navigator.of(context).pushReplacementNamed("/"),
+        onTap: () {
+          print("${Globals.isCustomThemeSet.getValue()}");
+        },
         child: settingsTile(
           leading: Container(
             height: cardH,
@@ -180,7 +238,6 @@ class _SettingsMobilePortraitState extends State<SettingsMobilePortrait> {
           ),
           SizedBox(height: SizeConfig.screenWidth * 0.05),
           Expanded(child: settingsOptions()),
-          Expanded(child: Container()),
           logOut(),
           SizedBox(height: SizeConfig.screenWidth * 0.025),
         ],
@@ -192,6 +249,11 @@ class _SettingsMobilePortraitState extends State<SettingsMobilePortrait> {
 class Setting {
   final String imagePath;
   final String title;
+  final String routePath;
 
-  Setting({@required this.imagePath, @required this.title});
+  Setting({
+    @required this.imagePath,
+    @required this.title,
+    @required this.routePath,
+  });
 }
